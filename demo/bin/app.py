f4f3fff91c6886716dd2350efd41e8ee4dc5d2ba
Peter Burgess <peter.a.burgess@gmail.com>
2016-09-30 11:08:01 +0000

    Ex 51
    Half way

--- demo/bin/app.py
+++ demo/bin/app.py
@@ -1,17 +1,23 @@
 import web
 
 urls = (
-	'/', 'index'
+  '/hello', 'Index'
 )
+
 
 app = web.application(urls, globals())
 
+
 render = web.template.render('/home/ubuntu/workspace/demo/templates/')
 
-class index:
-	def GET(self):
-		greeting = "Hello World"
-		return render.index(greeting = greeting)
+class Index(object):
+    def GET(self):
+        return render.hello_form()
+
+    def POST(self):
+        form = web.input(name="Nobody", greet="Hello")
+        greeting = "%s, %s" % (form.greet, form.name)
+        return render.index(greeting = greeting)
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
