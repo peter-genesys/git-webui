dc08821fd396627d2b7986845cec43b67833c350
Peter Burgess <peter.a.burgess@gmail.com>
2016-10-02 03:29:43 +0000

    seeother
    Added class redirect
    Added var tempates_dir
    Added function seeother

--- prototype/bin/app.py
+++ prototype/bin/app.py
@@ -5,16 +5,16 @@ import sys
 
 #from gothonweb import map
 
-urls = (
-  '/'        , 'Index'
- ,'/repolist', 'RepoList'
+urls = ('/(.*)/'   , 'redirect'   #redirect without trailing /
+       ,'/'        , 'Index'
+       ,'/repolist', 'RepoList'
 )
 
 app = web.application(urls, globals())
 
 #Start from app.py (python script and go up 2 dirs)
-web_root = os.path.dirname(os.path.dirname(os.path.realpath(sys.argv[0])))
-web.debug(web_root)
+#web_root = os.path.dirname(os.path.dirname(os.path.realpath(sys.argv[0])))
+#web.debug(web_root)
 #Then go down 3 dirs - this is the new root that web addresses will be related to.
 #Eg release/share/git-webui/webui/ (css|img|js)
 #web_root = os.path.join(web_root, "share", "git-webui", "webui")
@@ -30,19 +30,33 @@ web.debug(web_root)
 
 #render = web.template.render('templates/', base="layout")
 
-app_root = os.path.dirname(os.path.dirname(__file__))
+#Find the root of the app
+app_root = os.path.dirname(os.path.dirname(__file__))  #Start from app.py (python script and go up 2 dirs)
 web.debug(app_root)
-render = web.template.render(os.path.join(app_root,'templates/'), globals=globals(), base="layout")
+tempates_dir = os.path.join(app_root,'templates/')
+web.debug(tempates_dir)
+render = web.template.render(tempates_dir, globals=globals(), base="layout")
 
 #workaround for missing host details
 def full_url(path):
     return 'https://'+web.ctx.host+'/'+path
+    
+def seeother(path):
+    raise web.seeother(full_url(path))  
+    
+    
+class redirect:
+    def GET(self, path):
+        #This effectively removes the trailing / and retries it.
+        #raise web.seeother('https://'+web.ctx.host+'/' + path)
+        seeother(path)
 
 class Index(object):
     def GET(self):
         # this is used to "setup" the session with starting values
         #session.SQLsession = None
-        raise web.seeother(full_url('repolist'))  
+        seeother('repolist')
+        #raise web.seeother(full_url('repolist'))  
 
 
 class RepoList(object):
@@ -61,8 +75,9 @@ class RepoList(object):
         # there is a bug here, can you fix it?
         #if session.room and form.action:
         #    session.room = session.room.go(form.action)
-
-        raise web.seeother(full_url('repolist'))  
+  
+        seeother('repolist')
+        #raise web.seeother(full_url('repolist'))  
 
 if __name__ == "__main__":
     app.run()
